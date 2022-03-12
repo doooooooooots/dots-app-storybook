c59f4e7b1ee5257d97bfe198bf1badad4a9aac69
doooooooooots <doooooooooots@gmail.com>
2022-03-12 23:17:02 +0100

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { GlobalStyle } from '../src/shared/global';
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  )
+];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  a11y: {
+    // the target DOM element
+    element: '#root',
+    // sets the execution mode for the addon
+    manual: false
+  },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  }
+};
