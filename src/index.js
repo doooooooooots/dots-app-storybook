4dd8ade891d90c9c941418cc07ef579c7e42c530
doooooooooots <doooooooooots@gmail.com>
2022-03-13 11:37:29 +0100

    remove icons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import * as icons from "./shared/icons";
 
 export { icons };
 
-export * from "./components/icon/icon";
 export * from "./components/task-list/task";
 export * from "./components/action/action";
